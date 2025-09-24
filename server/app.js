5a66fa6abb7bf6133cbfe574737e15d0713a9506
theokyle <theokyle@gmail.com>
2025-09-24 11:26:43 -0500

    initialize express server and connect to mongodb

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+app.get("/", (request, response) => {
+  response.send("Welcome to the Primal Tracker API");
+});
+app.get("/status", (request, response) => {
+  response.json({ message: "Service healthy" });
+});
+
+const server = app.listen(port, () => console.log(`Listening on Port: ${server.address().port}`))
