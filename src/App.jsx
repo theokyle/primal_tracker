9fee887fef4d75766b6f48ed511f8fbc90c40241
theokyle <theokyle@gmail.com>
2025-09-26 15:33:26 -0500

    add nav links and player components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import Topbar from './components/layout/Topbar';
 import Navbar from './components/layout/Navbar';
 import { useState } from 'react';
+import { Outlet } from 'react-router';
 
 function App() {
   const [sideBarOpen, setSideBarOpen] = useState(false);
 
   return (
-    <div className="flex bg-gray-100 h-screen">
+    <div className="flex">
       <Navbar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen}/>
-      <main className='flex-1'>
-        <Topbar setSideBarOpen={setSideBarOpen} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4">
-          {/* Sample Card */}
-          <div className="bg-white p-6 shadow-lg rounded-lg">
-            <h2 className="text-xl font-bold">Card</h2>
-            <p className="text-lg p-1 text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero tortor, tempus non enim at, euismod tristique odio. Phasellus placerat magna eget viverra pellentesque.
-            </p>
-          </div>
-        </div>
-      </main>
+      <Outlet setSideBarOpen={setSideBarOpen} />
     </div>
   )
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { GiCrestedHelmet } from "react-icons/gi";
 import { FaScroll } from "react-icons/fa";
 import { GiSpikedDragonHead } from "react-icons/gi";
@@ -6,25 +7,29 @@ import { FaGear } from "react-icons/fa6";
 
 export default function Navbar({sideBarOpen, setSideBarOpen}) {
 const navItems = [
-    {name: "Player", icon: <GiCrestedHelmet />},
-    {name: "Campaign", icon: <FaScroll />},
-    {name: "Encounter", icon: <GiSpikedDragonHead />},
-    {name: "Settings", icon: <FaGear />}
+    {name: "Player", icon: <GiCrestedHelmet />, path: "player"},
+    {name: "Campaign", icon: <FaScroll />, path: "campaign"},
+    {name: "Encounter", icon: <GiSpikedDragonHead />, path:"encounter"},
+    {name: "Settings", icon: <FaGear />, path:"settings"}
 ];
 
     return (
         <nav className={`fixed bg-white w-64 h-screen shadow ${sideBarOpen ? "translate-x-0" : "-translate-x-64"} lg:translate-x-0 lg:static`}>
             <div className="p-4 flex justify-between border-b">
-                <div className="text-xl font-bold">Logo</div>
+                <Link to="/"><div className="text-xl font-bold"><img src="src/assets/primal_tracker_logo.png"></img></div></Link>
                 <button className="lg:hidden" onClick={() => setSideBarOpen(false)}>X</button>
             </div>
-            <div className="p-4 space-y-2">
+            <div className="p-4 space-y-4">
                 {navItems.map( item => {
                     return (
-                        <div key={item.name} className="flex p-2 hover:bg-gray-100 space-x-3">
-                            <div className="text-3xl">{item.icon}</div>
-                            <div className="text-xl">{item.name}</div>
-                            </div>
+                        <div key={item.name}>
+                            <Link to={item.path}>
+                                <div className="flex p-3 hover:bg-gray-100 space-x-3">
+                                    <div className="text-3xl">{item.icon}</div>
+                                    <div className="text-xl">{item.name}</div>
+                                </div>
+                            </Link>
+                        </div>
                     )
                 })}
             </div>
